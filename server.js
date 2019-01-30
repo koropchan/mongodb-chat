10c80af18c91eccc0953e9a442806e6a56380624
Stan <koropchan.sv@gmail.com>
2019-01-30 16:57:42 -0500

    Server issues are fixed

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
   });
 
   
-//Connecting to Mongo
+//Connecting to Mongo. Passing URI with enviroment var
 mongo.connect(process.env.MONGOLAB_URI, (err, db) => {
     if(err){
         console.log(err);
@@ -71,76 +71,3 @@ mongo.connect(process.env.MONGOLAB_URI, (err, db) => {
         });
     });
 });
-
-
-
-// const mongo = require('mongodb').MongoClient;
-// const client = require('socket.io').listen(4000).sockets;
-// require('dotenv').config();
-
-// //Importing envirometal var from '.env' file
-// var url = {
-//     host: process.env.DB_HOST,
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASS
-//   };
-
-// //Connecting to Mongo
-// mongo.connect(url, (err, db) => {
-//     if(err){
-//         console.log(err);
-//     }
-
-//     console.log('Database is connected..');
-
-//     //Connecting to socket.io
-//     client.on('connection', (socket) => {
-//         let chat = db.collection('chats');
-
-//         //Creating function to set status
-//         sendStatus = (s) => {
-//             socket.emit('status', s); // Using 'emit' to pass from server to client ('name', parametr)
-//         }
-
-//         //Fetching chats (messages) then taking the result (res) that has been sent back 
-//         //and then emitted(.emit) to the client to 'index' file
-//         chat.find().limit(100).sort({_id:1}).toArray((err, res) => {
-//             if(err){
-//                 err;
-//             }
-//             socket.emit('output', res);
-//         });
-
-//         //Handle input events
-//         socket.on('input', (data) => {
-//             let name = data.name;
-//             let message = data.message;
-
-//             //Cheking for name and message
-//             if(name == '' || message == ''){
-//                 //Sending error satus
-//                 sendSatus('Please enter name and status');
-//             } else {
-//                 //Insert message
-//                 chat.insert({name: name, message: message}, () => {
-//                     client.emit('output', [data]);
-
-//                     //Sending status object 
-//                     sendStatus({
-//                         message: 'Message sent',
-//                         clear: true
-//                     });
-//                 });
-//             }
-//         });
-
-//         //Handle 'clear'
-//         socket.on('clear', (data) => {
-//             //Removing all chats from collection
-//             chat.remove({}, () => {
-//                 //Emit cleared
-//                 socket.emit('cleared');
-//             });
-//         });
-//     });
-// });
